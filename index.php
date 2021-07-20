2f8a0c068b0f419080d09f4027b393d2886df06d
Piotr_Pustul <piotrpustul1@gmail.com>
2021-07-20 22:15:57 +0100

    routing code optimization with switch in index.php

--- index.php
+++ index.php
@@ -10,26 +10,34 @@ require_once("src/View.php");
 const DEFAULT_ACTION = 'list';
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
-
 $view = new View();
 
 $viewParams = [];
-if ($action === 'create') {
-   $page = 'create';
-   $created = false;
+switch ($action) {
+   case 'create':
+      $page = 'create';
+      $created = false;
 
-   if (!empty($_POST)) {
-      $created = true;
+      if (!empty($_POST)) {
+         $created = true;
+         $viewParams = [
+            'title' => $_POST['title'],
+            'description' => $_POST['description']
+         ];
+      }
+
+      $viewParams['created'] = $created;
+      break;
+   case 'show':
       $viewParams = [
-         'title' => $_POST['title'],
-         'description' => $_POST['description']
+         'title' => 'My Title',
+         'description' => 'My Description'
       ];
-   }
-
-   $viewParams['created'] = $created;
-} else {
-   $page = 'list';
-   $viewParams['resultList'] = 'Wyswietlamy notatki';
+      break;
+   default:
+      $page = 'list';
+      $viewParams['resultList'] = 'Wyswietlamy notatki';
+      break;
 }
 
 $view->render($page, $viewParams);
