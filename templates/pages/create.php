17f379d9886130f5835d944b3f4cd9d8d0652609
Piotr_Pustul <piotrpustul1@gmail.com>
2021-07-20 21:23:09 +0100

    worked on displaying data in a view

--- templates/pages/create.php
+++ templates/pages/create.php
@@ -1,21 +1,27 @@
 <div>
    <h3> New Note </h3>
    <div>
-      <?php dump($params); ?>
-      <form class="note-form" action="/?action=create" method="post">
-         <ul>
-            <li>
-               <label>Title <span class="required">*</span></label>
-               <input type="text" name="title" class="field-long">
-            </li>
-            <li>
-               <label>Description <span class="required">*</span></label>
-               <textarea name="description" id="field5" class="field-long field-textarea"></textarea>
-            </li>
-            <li>
-               <input type="submit" value="Submit">
-            </li>
-         </ul>
-      </form>
+      <?php if ($params['created']) : ?>
+         <div>
+            <div>Title: <?php echo $params['title'] ?></div>
+            <div>Description: <?php echo $params['description'] ?></div>
+         </div>
+      <?php else : ?>
+         <form class="note-form" action="/?action=create" method="post">
+            <ul>
+               <li>
+                  <label>Title <span class="required">*</span></label>
+                  <input type="text" name="title" class="field-long">
+               </li>
+               <li>
+                  <label>Description <span class="required">*</span></label>
+                  <textarea name="description" id="field5" class="field-long field-textarea"></textarea>
+               </li>
+               <li>
+                  <input type="submit" value="Submit">
+               </li>
+            </ul>
+         </form>
+      <?php endif; ?>
    </div>
 </div>
